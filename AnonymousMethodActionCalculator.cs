04c940d532bde61c93ca55d24803c8f191215049
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-07 11:42:31 +0530

    Add files via upload

---
+++ AnonymousMethodActionCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AnonymousMethodActionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> addNum = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine( firstNumber + secondNumber);
+            };
+
+            Action<int, int> subtractNum = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine(firstNumber - secondNumber);
+            };
+
+            Action<int, int> productNum = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine(firstNumber * secondNumber);
+            };
+
+            Action<int, int> divisionNum = delegate (int firstNumber, int secondNumber)
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            };
+
+            Calculator(addNum+=subtractNum+=productNum+=divisionNum,10, 3);
+
+       
+    }
+    static void Calculator(Action<int, int> cal, int firstNumber, int secondNumber)
+    {
+        cal(firstNumber, secondNumber);
+    }
+}
+}
